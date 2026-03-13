88f571759c9d535587638fa8a41d18ac04819475
Demarcus7776 <gusmar655@gmail.com>
2026-03-13 17:13:45 +0000

    u n me

--- structrs/ts_0.c
+++ structrs/ts_0.c
@@ -1,7 +1,37 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-struct phone
+char	ft_strcpy(char *str , char *dst)
 {
-	char name[];
-	int
+	int i;
+	
+	i  = 0;
+	while(str[i])
+	{
+		dst[i] = str[i];
+		i++;
+	}
+	
+	return *dst;
+}
+
+struct gamer
+{
+	char	name[50];
+	int	lvl;
+	float	health;
+};
+
+int main ()
+{
+	struct gamer s1;
+
+	ft_strcpy("lebron", s1.name);
+	s1.lvl = 67;
+	s1.health = 19.22;
+
+	printf("%s\n", s1.name);
+	printf("%d\n", s1.lvl);
+	printf("%f\n", s1.health);
+
 }
