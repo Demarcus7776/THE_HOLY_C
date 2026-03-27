612edb0e5ebd76f42bf2ba9503b68654ab8d89d7
Demarcus7776 <gusmar655@gmail.com>
2026-03-27 17:59:26 +0100

    mhm

--- array/arr4.c
+++ array/arr4.c
@@ -1,3 +1,36 @@
 #include <stdio.h>
 
-int	ft_is
+int	if_sorted(int *arr, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while(i < size)
+	{
+		j = i + 1;
+		while(j < size)
+		{
+			if(i < j)
+				return 1;
+			j++;
+		}
+		i++;
+	}
+
+	return 0;
+}
+
+int main ()
+{
+	int arr[] = {1,2,3,4,5};
+
+	if(if_sorted)
+	{
+		printf("this shit is sorted");
+	}
+	else
+	{
+	
+	}
+}
